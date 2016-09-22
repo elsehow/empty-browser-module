9d86068c22d8171179a781d7bb9d2752b44de671
elsehow <yes@cosmopol.is>
2016-09-22 16:00:19 -0700

    updates to view state

--- src/index.js
+++ src/index.js
@@ -1,1 +1,108 @@
-module.exports = "hello"
+var yo = require('yo-yo')
+/*
+   example state:
+   {
+     last_observed: [
+       { time: 'iso time',
+       label: 'some label' },
+       ....
+     ],
+     predicted: {
+       'Moves off': 220,
+       'walking': 1,
+       'cycling': 2,
+       'Unknown place': 2,
+       'South Hall': 1,
+       "Noura's house": 2
+     }
+   }
+*/
+
+const WIDTH   = 300
+const PADDING = 5
+const MARGIN  = 3
+const COLORS = [
+  "#7100FA",
+  "#C614C7",
+  "#943B94",
+  "#FAE43E",
+  "aliceblue",
+  "blueviolet",
+  "cadetblue",
+  "chartreuse",
+  "crimson",
+  "DarkGoldenRod"
+]
+
+function observed (moves) {
+  return yo`<div>
+    ${moves.map(function (item) {
+      return yo`<div style=
+        "border:  #222 1px solid;
+         width:   ${WIDTH/3}px;
+         padding: ${PADDING}px;
+         margin:  auto;">
+        ${item.label}
+        <span style="
+           font-size:8pt;
+           color:#ccc;
+          ">
+          ${item.time}
+        </span>
+      </div>`
+    })}
+  </div>`
+}
+
+function pieBar (proportions) {
+  function proportionalBar (pred) {
+    return yo`<div>
+      <div>
+      ${pred.label} 
+    </div>
+      <div style=
+        "width:      ${(WIDTH/9)*pred.percent}px;
+         background: ${pred.color};
+         border:     #222 1px solid;
+         height:     30px;
+         float:      left;
+         margin:     0 20px 0 0;
+         ">
+      </div>
+    </div>`
+  }
+  return yo`<div>
+    ${proportions.map(proportionalBar)}
+  </div>`
+}
+
+function predicted (prediction) {
+  let sum = Object.keys(prediction)
+      .reduce((acc, cur) => acc+=prediction[cur], 0)
+  let proportions = Object.keys(prediction)
+      .map((cur, i)=> {
+        return {
+          label:   cur,
+          percent: prediction[cur]/sum,
+          color:   COLORS[i],
+        }
+      })
+  return yo`<div style=
+    "margin: auto;">
+    ${pieBar(proportions)}
+  </div>`
+  // ${legend(proportions)}
+}
+
+function view (state) {
+  return yo`<div style="
+   width: ${WIDTH}px;
+   margin: auto;
+   ">
+    <div>
+    ${observed(state.last_observed)}
+    ${predicted(state.prediction)}
+  </div>`
+}
+
+module.exports = view
--- test/index.js
+++ test/index.js
@@ -1,8 +1,20 @@
 var ud = require('ud');
 
+let state ={ last_observed: 
+             [ { time: '5:14 PM', label: 'Unknown place' },
+               { time: '5:24 PM', label: 'walking' },
+               { time: '5:34 PM', label: 'walking' },
+               { time: '5:44 PM', label: 'walking' },
+               { time: '5:54 PM', label: 'walking' },
+               { time: '6:04 PM', label: 'walking' },
+               { time: '6:14 PM', label: 'walking' } ],
+             prediction: { Home: 1, walking: 1 } }
+
 // everything in this function will get updated on change
-var setup = ud.defn(module, function (state) {
-  console.log(require('..'))
+var setup = ud.defn(module, function () {
+  let el = require('..')(state)
+  document.body.innerHTML=''
+  document.body.appendChild(el)
 })
 
 // will re-run setup() whenever method changes
